23b04af2ef56b9e447351bc98fe65f204cc52403
jake waters <jerkey@g-cipher.net>
2016-03-22 16:48:06 -0700

    non-blocking data window can be closed

--- celltest.py
+++ celltest.py
@@ -1,7 +1,18 @@
 from recorder import *
+import time
 SR = SwhRecorder()
 SR.setup()
 
-SR.record(forever=False)
-pylab.plot(SR.audio.flatten())
-pylab.show()        
+SR.record(forever=False) # record one buffer's worth of audio
+
+pylab.plot(SR.audio.flatten()) # create the plot of the data
+
+pylab.ion() # use interactive aka Non-blocking mode for .show()
+
+pylab.show() # bring up the data window (non-blocking)
+
+time.sleep(2) # wait 2 seconds
+
+pylab.close() # close the data window
+
+time.sleep(2) # wait 2 seconds
