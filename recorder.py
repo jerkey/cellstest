011e3efeb4d96c7bba75b0f6a6e5c5676e763f28
jake waters <jerkey@g-cipher.net>
2016-10-14 17:41:20 -0700

    convert recorder.py from dos to unix, remove trailing whitespace

--- recorder.py
+++ recorder.py
@@ -12,7 +12,7 @@ import json
 
 class SwhRecorder:
     """Simple, cross-platform class to record from the microphone."""
-    
+
     def __init__(self,device):
         """minimal garb is executed when class is loaded."""
         self.RATE=48100
@@ -21,7 +21,7 @@ class SwhRecorder:
         self.threadsDieNow=False
         self.newAudio=False
         self.input_device_index=device
-        
+
     def setup(self):
         """initialize sound card."""
         #TODO - windows detection vs. alsa or something for linux
@@ -32,7 +32,7 @@ class SwhRecorder:
         self.samplesToRecord=int(self.BUFFERSIZE*self.buffersToRecord)
         self.chunksToRecord=int(self.samplesToRecord/self.BUFFERSIZE)
         self.secPerPoint=1.0/self.RATE
-        
+
         self.p = pyaudio.PyAudio()
         # http://forum.cogsci.nl/index.php?p=/discussion/287/open-two-sound-cards/p1
         count = self.p.get_device_count()
@@ -59,52 +59,52 @@ class SwhRecorder:
             rate=self.RATE,
             input=True,
             frames_per_buffer=self.BUFFERSIZE)
-        
+
         self.xsBuffer=numpy.arange(self.BUFFERSIZE)*self.secPerPoint
         self.xs=numpy.arange(self.chunksToRecord*self.BUFFERSIZE)*self.secPerPoint
-        self.audio=numpy.empty((self.chunksToRecord*self.BUFFERSIZE),dtype=numpy.int16)               
-    
+        self.audio=numpy.empty((self.chunksToRecord*self.BUFFERSIZE),dtype=numpy.int16)
+
     def close(self):
         """cleanly back out and release sound card."""
         self.p.close(self.inStream)
-    
-    ### RECORDING AUDIO ###  
-    
+
+    ### RECORDING AUDIO ###
+
     def getAudio(self):
         """get a single buffer size worth of audio."""
         audioString=self.inStream.read(self.BUFFERSIZE)
         return numpy.fromstring(audioString,dtype=numpy.int16)
-        
+
     def record(self,forever=True):
         """record secToRecord seconds of audio."""
         while True:
             if self.threadsDieNow: break
             for i in range(self.chunksToRecord):
                 self.audio[i*self.BUFFERSIZE:(i+1)*self.BUFFERSIZE]=self.getAudio()
-            self.newAudio=True 
+            self.newAudio=True
             if forever==False: break
-    
+
     def continuousStart(self):
         """CALL THIS to start running forever."""
         self.t = threading.Thread(target=self.record)
         self.t.start()
-        
+
     def continuousEnd(self):
         """shut down continuous recording."""
         self.threadsDieNow=True
 
     ### MATH ###
-            
+
     def downsample(self,data,mult):
         """Given 1D data, return the binned average."""
         overhang=len(data)%mult
         if overhang: data=data[:-overhang]
         data=numpy.reshape(data,(len(data)/mult,mult))
         data=numpy.average(data,1)
-        return data    
-        
+        return data
+
     def fft(self,data=None,trimBy=10,logScale=False,divBy=100):
-        if data==None: 
+        if data==None:
             data=self.audio.flatten()
         left,right=numpy.split(numpy.abs(numpy.fft.fft(data)),2)
         ys=numpy.add(left,right[::-1])
@@ -118,11 +118,11 @@ class SwhRecorder:
         if divBy:
             ys=ys/float(divBy)
         return xs,ys
-    
+
     ### VISUALIZATION ###
-    
+
     def plotAudio(self):
         """open a matplotlib popup window showing audio data."""
         pylab.plot(self.audio.flatten())
-        pylab.show()        
-            
+        pylab.show()
+
