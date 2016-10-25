c703651f295bed93e17ff0a38398833125e2914d
jake waters <jerkey@g-cipher.net>
2016-10-25 00:49:38 -0700

    finally got it working with two channels

--- recorder.py
+++ recorder.py
@@ -15,8 +15,9 @@ class SwhRecorder:
 
     def __init__(self,device):
         """minimal garb is executed when class is loaded."""
-        self.RATE=48100
-        self.BUFFERSIZE=2**12 #1024 is a good buffer size
+        self.RATE=44100
+        self.channels=2
+        self.BUFFERSIZE=2**13 #1024 is a good buffer size
         self.secToRecord=.1
         self.threadsDieNow=False
         self.newAudio=False
@@ -55,14 +56,14 @@ class SwhRecorder:
 
         self.inStream = self.p.open(format=pyaudio.paInt16,
             input_device_index=self.input_device_index,
-            channels=1,
+            channels=self.channels,
             rate=self.RATE,
             input=True,
             frames_per_buffer=self.BUFFERSIZE)
 
-        self.xsBuffer=numpy.arange(self.BUFFERSIZE)*self.secPerPoint
-        self.xs=numpy.arange(self.chunksToRecord*self.BUFFERSIZE)*self.secPerPoint
-        self.audio=numpy.empty((self.chunksToRecord*self.BUFFERSIZE),dtype=numpy.int16)
+        #self.xsBuffer=numpy.arange(self.BUFFERSIZE)*self.secPerPoint
+        #self.xs=numpy.arange(self.chunksToRecord*self.BUFFERSIZE)*self.secPerPoint
+        self.audio=numpy.empty((self.chunksToRecord*self.BUFFERSIZE*self.channels),dtype=numpy.int16)
 
     def close(self):
         """cleanly back out and release sound card."""
@@ -80,7 +81,7 @@ class SwhRecorder:
         while True:
             if self.threadsDieNow: break
             for i in range(self.chunksToRecord):
-                self.audio[i*self.BUFFERSIZE:(i+1)*self.BUFFERSIZE]=self.getAudio()
+                self.audio[i*self.BUFFERSIZE:(i+1)*self.BUFFERSIZE*self.channels]=self.getAudio()
             self.newAudio=True
             if forever==False: break
 
